0274309ff04e2f586b1b7018f3d55e10384f57c7
Spencer Scorcelletti <asimpletune@users.noreply.github.com>
2017-12-04 11:07:35 -0800

    Add argument parser for CLI to main

--- main.go
+++ main.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"github.com/docopt/docopt-go"
+	"log"
+	"strconv"
 )
 
 func main() {
-	quit := make(chan int)
-	done := make(chan int)
+	filePath, interval := parseArgs()
+	quit, done := beginMonitoring(NewController(filePath, interval))
+	handleSignals(quit, done)
+}
+
+func handleSignals(quit chan int, done chan int) {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
-
-	controller := NewController("/Users/Spencer/Desktop/server/access.log", 3)
-	go controller.Monitor(quit, done)
-
 	for range signalChan {
 		fmt.Println("sending quit signal")
 		quit <- 1
@@ -24,3 +27,44 @@ func main() {
 		break
 	}
 }
+
+func beginMonitoring(controller *Controller) (quit chan int, done chan int) {
+	q := make(chan int)
+	d := make(chan int)
+
+	go controller.Monitor(q, d)
+
+	return q, d
+}
+
+func parseArgs() (filePath string, interval int) {
+	var err error
+	if arguments, err := docopt.Parse(usage, nil, true,  version, false); err == nil {
+		if timeString, _ := arguments["--time"]; timeString != nil {
+			fmt.Println(arguments)
+			if timeSeconds, err := strconv.Atoi(timeString.(string)); err == nil {
+				if  timeSeconds <= 0 {
+					timeSeconds = defaultInterval
+				}
+				return arguments["<file>"].(string), timeSeconds
+			}
+		} else {
+			return arguments["<file>"].(string), defaultInterval
+		}
+	}
+	log.Fatal(err)
+	return
+}
+
+const version = "HTTP Log Monitoring Console Program 0.1"
+const defaultInterval = 10
+const usage = `HTTP Log Monitoring Console Program.
+
+Usage:
+  monitor [--time=10] <file>
+
+Options:
+  -h --help     		Show this screen.
+  --version     		Show version.
+  -t --time=<seconds>	Time to refresh log stats in seconds [default: 10].`
+
